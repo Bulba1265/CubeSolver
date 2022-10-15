bdc48b66cc9a6dc834fff5260826ccaaea96ff48
gruby1265 <58722490+gruby1265@users.noreply.github.com>
2022-10-15 09:02:59 +0200

    scales added

--- RotatingSim.py
+++ RotatingSim.py
@@ -3,6 +3,18 @@ import math
 import numpy
 
 
+def ax_change(value):
+    global ax, last_ax_value
+    ax += 0.01*(int(value) - last_ax_value)
+    last_ax_value = int(value)
+
+
+def ay_change(value):
+    global ay, last_ay_value
+    ay += 0.01*(int(value) - last_ay_value)
+    last_ay_value = int(value)
+
+
 def get_position(x, y, z):
     angle = numpy.arctan2(y, x)
     #print(numpy.rad2deg(angle), y, x)
@@ -33,10 +45,7 @@ def get_depth(angle, r, h):
 
 def draw_cube():
     canvas.delete("all")
-    global ax
-    global ay
-    ax += 0.03
-    ay -= 0.03
+
     vertexes = [(0, 0) for i in range(0, 8)]
     vertexes[0] = draw_point(1, 1, 1)
     vertexes[1] = draw_point(1, -1, 1)
@@ -93,8 +102,22 @@ window = Tk()
 window.resizable(False, False)
 window.title("Rotiting Cube")
 
-canvas = Canvas(window, bg="#000000", height=700, width=1000)
-canvas.pack()
+label_x = Label(text="X", font=("Courier", 20), fg='#73B5FA', justify=CENTER)
+label_x.grid(column=0, row=0)
+
+label_y = Label(text="Y", font=("Courier", 20), fg='#90ee90', justify=RIGHT)
+label_y.grid(column=1, row=0)
+
+scale_x = Scale(from_=-400, to=400, length=300, troughcolor='#73B5FA', command=ax_change)
+scale_x.grid(column=0, row=1)
+
+scale_y = Scale(from_=-400, to=400, length=300, troughcolor='#90ee90',command=ay_change)
+scale_y.grid(column=1, row=1)
+
+canvas = Canvas(window, bg="#333333", height=500, width=800)
+canvas.grid(column=2, row=1)
+
+
 window.update()
 
 window_height = window.winfo_height()
@@ -107,7 +130,8 @@ y_temp = int((screen_height/2)-(window_height/2))
 #print(window_width, window_height, x, y, screen_width, screen_height)
 window.geometry(f"{window_width}x{window_height}+{x_temp}+{y_temp}")
 
-
+last_ax_value = 0
+last_ay_value = 0
 ax = 1
 ay = 1
 d = 7
